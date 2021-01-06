9cbb6de6f56ef7151145cb52f63fd45ba1dd042b
gsingh-0-0-1 <gurmehar@gmail.com>
2021-01-06 07:28:44 +0000

    first stable version

--- main.js
+++ main.js
@@ -2,14 +2,51 @@ const express = require('express')
 var fs = require('fs');
 const {URLSearchParams} = require('url')
 
+const redis = require("redis");
+const client = redis.createClient(6379, 'redishost');
+
 const app = express()
-const port = 80
-const host = 'localhost'
+const port = 81
+const host = '0.0.0.0'
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
 	res.sendFile("public/templates/main.html", {root: __dirname})
+})
+
+app.get("/node", (req, res) => {
+	res.sendFile("public/templates/node.html", {root: __dirname})
+})
+
+app.get("/allkeys", (req, res) => {
+	client.keys("*", function(err, reply) {
+		res.send(reply)
+	});
+})
+
+app.get("/basics", (req, res) => {
+	var url = req.url.split("?")[1]
+	urlParams = new URLSearchParams(url)
+
+	var nodenum = urlParams.get("nodenum")
+	var nodename = "hashpipe://seti-node" + nodenum[0] + "/" + nodenum[1] + "/status"
+
+	client.hmget(nodename, "DAQPULSE", "SYNCTIME", function(err, reply){
+		res.send(reply.toString().replace("[", "").replace("]", ""))
+	});
+})
+
+app.get("/getall", (req, res) => {
+	var url = req.url.split("?")[1]
+	urlParams = new URLSearchParams(url)
+
+	var nodenum = urlParams.get("nodenum")
+	var nodename = "hashpipe://seti-node" + nodenum[0] + "/" + nodenum[1] + "/status"
+
+	client.hgetall(nodename, function(err, reply){
+		res.send(reply)
+	})
 })
 
 app.listen(port, host)
