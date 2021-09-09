bfaf09774b9a86388b43b76b05740f008b86c3a4
RocketRoss <code@radonn.co.za>
2021-09-09 10:05:22 +0000

    *+ main fetchBasics JSON, summary_keys.txt

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ const app = express()
 const port = 81
 const host = '0.0.0.0'
 
+let summary_keys = []
+fs.readFile('summary_keys.txt', (err, data) => {
+	if (err) throw err;
+	summary_keys = data.toString().split("\n")
+	console.log(summary_keys)
+})
+
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
@@ -29,13 +36,18 @@ app.get("/basics", (req, res) => {
 	var url = req.url.split("?")[1]
 	urlParams = new URLSearchParams(url)
 
-	var nodenum = urlParams.get("nodenum")
-	var instancenum = urlParams.get("instancenum")
-	var nodename = "hashpipe://seti-node" + nodenum + "/" + instancenum + "/status"
-
-	client.hmget(nodename, "DAQPULSE", "SYNCTIME", "DAQSTATE", "PHYSGBPS", "IBVGBPS", "ANTNAMES", "OBSNDROP", "PPSTATUS", "POSTPROC", "OBSINFO", function(err, reply){
-		res.send(reply)
-	});
+	let nodenum = urlParams.get("nodenum")
+	let instancenum = urlParams.get("instancenum")
+	let nodename = "hashpipe://seti-node" + nodenum + "/" + instancenum + "/status"
+	client.hmget(nodename, summary_keys,
+		function(err, reply){
+			let keyvalues = {};
+			for (let index = 0; index < summary_keys.length; index++) {
+				keyvalues[summary_keys[index]] = reply[index] === null ? "null" : reply[index];
+			}
+			res.send(keyvalues)
+		}
+	);
 })
 
 app.get("/getall", (req, res) => {
